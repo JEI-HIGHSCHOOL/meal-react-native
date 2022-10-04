7509441825eccc401f547d4b6ea04918ffc8cbb2
kiss8981 <kdh89811@gmail.com>
2022-10-04 12:19:13 +0900

    fix: date

--- utils/date.js
+++ utils/date.js
@@ -1,9 +1,10 @@
 export const getDate = date => {
   let getMonth;
   let getDate;
-  if (date.getMonth() < 10) getMonth = "0" + Number(date.getMonth() + 1);
+  if ((date.getMonth() + 1) < 10) getMonth = "0" + Number(date.getMonth() + 1);
   else getMonth = Number(date.getMonth() + 1);
-  if (date.getDate() < 10) getDate = "0" + Number(date.getDate());
+  if ((date.getDate() + 1) < 10) getDate = "0" + Number(date.getDate());
   else getDate = Number(date.getDate());
+  console.log(date.getFullYear() + getMonth.toString() + getDate.toString())
   return date.getFullYear() + getMonth.toString() + getDate.toString();
 };
